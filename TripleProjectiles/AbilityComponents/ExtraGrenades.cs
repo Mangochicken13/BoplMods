56531aa8aefc0ab49290e545b18eb64d2f1fac07
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2024-04-21 23:30:10 +0800

    Halfway to a grenade implementation

---
+++ TripleProjectiles/AbilityComponents/ExtraGrenades.cs
@@ -0,0 +1,75 @@
+﻿using BoplFixedMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TripleProjectiles.AbilityComponents
+{
+    public class ExtraGrenades : MonoBehaviour
+    {
+        public ThrowItem2 parent;
+
+        public Grenade leftGrenade;
+        public BoplBody leftGrenadeBody;
+        public Grenade rightGrenade;
+        public BoplBody rightGrenadeBody;
+
+        public void InitializeGrenades(Grenade grenade, BoplBody body, bool right)
+        {
+            if (right)
+            {
+                rightGrenade = grenade;
+                rightGrenadeBody = body;
+            }
+            else
+            {
+                leftGrenade = grenade;
+                leftGrenadeBody = body;
+            }
+        }
+
+        public void FireGrenade(Fix angleBetween, bool right)
+        {
+            Grenade grenade;
+            BoplBody grenadeBody;
+            Vec2 vec;
+            if (right)
+            {
+                grenade = rightGrenade;
+                grenadeBody = rightGrenadeBody;
+                vec = Helpers.RotateBy(parent.dir, angleBetween);
+            }
+            else
+            {
+                grenade = leftGrenade;
+                grenadeBody = leftGrenadeBody;
+                vec = Helpers.RotateBy(parent.dir, -angleBetween);
+            }
+
+            //this.dummy.SetActive(false);
+            //this.aimIndicator.SetActive(false);
+            Vec2 vec2 = parent.body.position + Vec2.ComplexMul(new Vec2(parent.body.rotation), parent.player.Scale * (parent.isGroundedThrow ? parent.gThrowPosition : parent.aThrowPosition) * (Fix)((parent.transform.localScale.x < 0f) ? (-1L) : 1L));
+            if (grenade != null && !grenade.hasBeenThrown)
+            {
+                grenade.DelayedEnableHurtOwner(parent.DisableGrenadeHitBoxTime * Fix.Max(parent.body.fixtrans.Scale, Fix.One));
+                grenadeBody.GetComponent<DPhysicsCircle>().ManualInit();
+                grenadeBody.position = vec2;
+                grenade.hasBeenThrown = true;
+                grenadeBody.UpdateSim(Fix.Zero);
+                Fix fix = Fix.Lerp(parent.minThrowForce, parent.maxThrowForce, Fix.Clamp01(parent.FireInputTimeStamp * parent.ThrowForceGainSpeed));
+                grenadeBody.AddForce(parent.body.fixtrans.Scale * fix * vec, ForceMode2D.Impulse);
+                //grenadeBody.AddForce(vec * (Fix)10);
+                //this.timeSinceFired = (Fix)0L;
+                parent.body.selfImposedVelocity -= parent.knockback * vec;
+                parent.physics.gravity_modifier = (Fix)1L;
+                //this.hasFired = true;
+                //AudioManager.Get().Play("throwSmallItem");
+                grenade.UpdateSim(grenade.detonationTime - grenade.selfDestructDelay);
+                return;
+            }
+        }
+    }
+}
---
+++ TripleProjectiles/Patches/GrenadePatch.cs
@@ -0,0 +1,83 @@
+﻿using BoplFixedMath;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripleProjectiles.AbilityComponents;
+using UnityEngine;
+
+namespace TripleProjectiles.Patches
+{
+    [HarmonyPatch]
+    public class GrenadePatch
+    {
+        static readonly int NumOfGrenades = 3;
+        static readonly Fix AngleBetween = (Fix)18;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ThrowItem2), nameof(ThrowItem2.Awake))]
+        public static void AddExtraGrenadeReferences(ThrowItem2 __instance)
+        {
+            if (!TripleProjectiles.IsFullGame)
+            {
+                return;
+            }
+
+            var cp = __instance.gameObject.AddComponent<ExtraGrenades>();
+            cp.parent = __instance;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ThrowItem2), nameof(ThrowItem2.SpawnGrenade))]
+        public static void SpawnExtraGrenades(ThrowItem2 __instance)
+        {
+            if (!TripleProjectiles.IsFullGame)
+            {
+                return;
+            }
+
+            var cp = __instance.gameObject.GetComponent<ExtraGrenades>();
+            for (int i = 0; i < NumOfGrenades; i++)
+            {
+                if (i == (NumOfGrenades - 1) / 2)
+                {
+                    continue;
+                }
+                var grenadeBody = FixTransform.InstantiateFixed<BoplBody>(__instance.ItemPrefab, __instance.dummyPos(), __instance.body.rotation);
+                grenadeBody.Scale = __instance.player.Scale;
+                Item component = grenadeBody.GetComponent<Item>();
+                component.OwnerId = __instance.playerInfo.playerId;
+                var grenade = component.GetComponent<Grenade>();
+                grenade.Initialize(__instance);
+                grenade.DetonatesOnOwner = false;
+                //this.dummy.SetActive(false);
+
+                cp.InitializeGrenades(grenade, grenadeBody, i == 0);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ThrowItem2), nameof(ThrowItem2.Fire))]
+        public static void ShootExtraGrenades(ThrowItem2 __instance)
+        {
+            if (!TripleProjectiles.IsFullGame)
+            {
+                return;
+            }
+
+            var cp = __instance.gameObject.GetComponent<ExtraGrenades>();
+            cp.FireGrenade(AngleBetween, true);
+            cp.FireGrenade(AngleBetween, false);
+        }
+
+        //[HarmonyPatch(typeof(Grenade), nameof(Grenade.UpdateSim))]
+        //[HarmonyPrefix]
+        public static void GrenadeLogger(Grenade __instance)
+        {
+            TripleProjectiles.Log.LogInfo(__instance.selfDestructDelay);
+        }
+
+    }
+}
